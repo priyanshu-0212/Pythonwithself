ca49d948c7b430fa13825eb0609cd17b69bc74e6
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-27 22:12:54 +0530

    Create string_slicing.py

---
+++ string_slicing.py
@@ -0,0 +1,17 @@
+fruit = "Banana"
+print("Banana is a",len(fruit),"letter word.")
+print(fruit[0:6]) #including 0 but not 6
+print(fruit[:6])
+print(fruit[0:3])
+print(fruit[::-1]) #reversing via slicing
+
+
+names = "Priyanshu, Aditya"
+print(len(names))
+print(names[0:9]) #including 0 but not 9
+print(names[11:17]) #including 11 but not 17
+print(names[::-1])
+
+# Quick quiz :
+nm = "Harry"
+print(nm[-4:-2])
