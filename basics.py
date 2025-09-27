ac55012589c8fcca9f0620cbad725044eb56d0a6
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-27 21:42:06 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,62 @@
+#everything in python is object as its OOPS and procedural both
+
+#comments and escape sequence & print statement
+""""Hey, Priyanshu don't remove this code
+as this are multiple line comments
+100 days of Code
+"""
+
+# print("Hey I am a \"good boy\",\n and this viewer is also a good boy/girl ")
+#
+# #this is a single line comment
+#
+# print("Hey", 6,7, sep="~", end=" ")
+# print("Priyanshu")
+
+
+
+#variables and data types
+
+a = 11 #integer
+b = "Priyanshu" #string
+c = True #boolean
+d = None #none
+e = 3.1 #float
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#adding two numbers
+a1 = 10
+print(a+a1) #arithmetic operation
+
+# data type of any variable
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
+
+
+#list and tuple
+#list is collection of different data type elements , lists are mutable
+#tuple is same as list but the difference is its immutable
+
+
+#mapped data in python consist of key value pair like every key will be have a value like in a dictionary
+
+#operators (+, adddition
+# -, = subtraction
+# *, = multiplication
+# /, = divide
+# //, = floor division
+# % , = modulus,
+# **, = exponential) = exponential means the first int will be base and the second int will be its power
+
+
+
+
+
