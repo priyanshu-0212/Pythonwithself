da6ff1393004da3021b5bb9846a1df974e2d783c
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-27 21:54:38 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,35 @@
+name = "Priyanshu"
+friend = "Aditya"
+another_friend = "Harry"
+
+print("Hello", name)
+print("Hello", friend)
+print("Hello", another_friend)
+
+#now if we want to do multi line strings we should use triple Inverted comma at the start and the ending of the string like
+
+apple = '''An apple a day,
+keeps a doctor away
+'''
+print(apple)
+
+#now lets work on the indexing of the given variable named as name
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+
+#print(name[9]) #this throws an error
+
+print("\n")
+# loops in strings
+print("For loop \n")
+
+for character in apple:
+    print(character)
