832dccef9afe1c816cf1265fac36ce62a6cd5348
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-27 21:40:23 +0530

    Create typecoversion.py

---
+++ typecoversion.py
@@ -0,0 +1,9 @@
+"""the conversion of one datatype into another data type is called typecasting
+but make sure the type conversion which you are doing should be valid
+two type of typecasting
+implicit = Jo python krra hai
+explicit = Jo hm krre hai
+"""
+a = "1"
+b = "2"
+print(int(a) + int(b)) #type conversion of string to integer
