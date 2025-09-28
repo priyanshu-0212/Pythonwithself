b3efeb898db2751f117afc7325f8f56181601c75
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-09-28 18:40:31 +0530

    Create casestatement.py

---
+++ casestatement.py
@@ -0,0 +1,10 @@
+x=int(input("Enter number: "))
+match x:
+    case 0:
+        print("case is 0")
+    case 1:
+        print("case is 1")
+    case 2:
+        print("case is 2")
+    case _:
+        print("error")
