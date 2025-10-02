b37c8ed25bfdb7ca5c0696f26e699920a7d851ec
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-10-03 03:50:35 +0530

    Create 02whileloop.py

---
+++ 02whileloop.py
@@ -0,0 +1,20 @@
+# #intitiating by giving i a value
+# i = 0
+# while i <= 3: #giving limit
+#     print(i) #printing i
+#     i = i+1 #increasing i by 1 otherwise it will get in infinite loop
+# print("End of loop") #for clearity writing it out from loop
+
+
+# i = int(input("Enter a number:"))
+# while i<=38:
+#     i = int(input("Enter a number:"))
+#     print(i)
+# print("Loop Ended")
+
+
+#decrement loop
+i = 5
+while i>0:
+    print(i)
+    i = i-1
