f5ada3e1fc1709abe2d0e6ee30ea3659e38eaf91
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2025-10-02 19:23:36 +0530

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,18 +1,30 @@
 #for loop in strings
-for i in range(10): #initiating for loop
-    print(i+1, end=" ")
+# for i in range(10): #initiating for loop
+#     print(i+1, end=" ")
+#
+# print("\n") #line changes with this
+#
 
-print("\n") #line changes with this
-
-
-name = "Priyanshu"
-for i in name:
-    print(i, end=" ")
-    if(i == "a"):
-        print("Yes its special")
+# name = "Priyanshu"
+# for i in name:
+#     print(i, end=" ")
+    # if(i == "a"):
+    #     print("Yes its special")
 
 #for loop in list
 
-colors = ["Red", "Green", "Blue", "Yellow", "Magenta", "Cyan"]
-for color in colors:
-    print(color, end=", ")
+# colors = ["Red", "Green", "Blue", "Yellow", "Magenta", "Cyan"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for k in range(5):
+#     print(k+1)
+#
+# for k in range(1,9):
+#     print(k)
+
+# defining ranges and telling the differences between all
+# for i in range(1,12,3):
+#     print(i)
